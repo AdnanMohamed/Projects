7122673a5b54b8e3d59325aacaad5e5dd763058c
Adnan <54870661+AdnanMohamed@users.noreply.github.com>
2020-05-10 20:54:20 +0300

    another way (avoiding nested if statements)

--- Text/fizzbuzz.py
+++ Text/fizzbuzz.py
@@ -6,12 +6,11 @@ For multiples of both three and five, the program will print "FizzBuzz"
 
 if __name__ == '__main__':
     for i in range(1, 101):
-        if(i % 3 == 0):
-            if(i % 5 == 0):
-                print "FizzBuzz"
-            else:
-                print "Fizz"
+        if(i % 3 == 0 and i % 5 == 0):
+            print "FizzBuzz"
         elif(i % 5 == 0):
             print "Buzz"
+        elif(i % 3 == 0):
+            print "Fizz"
         else:
             print i
